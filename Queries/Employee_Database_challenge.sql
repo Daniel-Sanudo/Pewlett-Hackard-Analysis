cbbf52d6a7e3571b8875a01651001beef80eea6f
Daniel-Sanudo <lebrieth@gmail.com>
2022-08-13 18:43:44 -0700

    Created mentorship elegibility table and query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -32,3 +32,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC
+
+-- Create the mentorship elegibility table
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	d.from_date,
+	d.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as d 
+	ON(e.emp_no = d.emp_no)
+INNER JOIN titles as t
+	ON(d.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
+	AND (d.to_date = ('9999-01-01'))
+ORDER BY e.emp_no;
